ab161c21e85cf5cfddf04dabbda9736da3e2e7b0
Shah Fahid <135055359+mrfahid@users.noreply.github.com>
2024-06-18 09:00:07 +0000

    change package.json

--- ecommerce-api/server.js
+++ ecommerce-api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const port = 3000;
+
+// Use the cors middleware
+app.use(cors());
 
 const products = [
   {
